6b1aa717264fc6d546358d0c8b78b157fdb97515
gulbahar-gunduzz <80460629+gulbahar-gunduzz@users.noreply.github.com>
2021-05-19 21:46:09 +0300

    Projenin Son Hali.

--- src/Book.java
+++ src/Book.java
@@ -27,8 +27,13 @@ public class Book {
         return book_name;
     }
 
-   
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
 
+    public String getBook_writer() {
+        return book_writer;
+    }
 
     public void setBook_writer(String book_writer) {
         this.book_writer = book_writer;
